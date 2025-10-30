97f3a6388c0c861ed7ab7b325fa820b3b265cfcb
SantiagoTabaresO <santiago.tabareso@udea.edu.co>
2025-10-30 10:23:45 -0500

    Add route for Resultados view

--- lis_frontend/src/router/index.ts
+++ lis_frontend/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Inicio from '../views/Inicio.vue'
 import Pacientes from '../views/Pacientes.vue'
+import Resultados from '../views/Resultados.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/inicio' },
     { path: '/inicio', name: 'Inicio', component: Inicio },
+    { path: '/resultados', component: Resultados },
     { path: '/pacientes', name: 'Pacientes', component: Pacientes }
   ]
 })
